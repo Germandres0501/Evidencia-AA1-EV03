bd4b744923520334b5298359e8db2be04e7d8214
GERMAN ANDRES CARRERO GARZON <gacarrero@soy.sena.edu.co>
2025-04-14 22:10:38 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import MiComponente from './MiComponente';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Mi Aplicación React</h1>
+                <MiComponente />
+            </header>
+        </div>
+    );
+}
+
+export default App;
+
+
---
+++ src/MiComponente.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MiComponente.css';
+
+function MiComponente() {
+    return (
+        <div className="mi-componente">
+            <h2>Este es mi primer componente</h2>
+            <p>¡Estoy aprendiendo React!</p>
+        </div>
+    );
+}
+
+export default MiComponente;
